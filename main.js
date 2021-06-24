6ad6d7b3f50f994d97fc95252dd7ad1056eae258
Jessseee <jssvssr2000@gmail.com>
2021-06-24 12:32:45 +0200

    Added startup arguments

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const { app, BrowserWindow, globalShortcut } = require('electron')
+const { app, BrowserWindow, globalShortcut, remote } = require('electron')
 require('update-electron-app')()
-
-app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow = null;
+
+// On OS X it is common for applications and their menu bar
+// to stay active until the user quits explicitly with Cmd + Q
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
 
 app.whenReady().then(() => {
     mainWindow = new BrowserWindow({
@@ -21,21 +25,19 @@ app.whenReady().then(() => {
         }
     });
 
-    mainWindow.loadURL('https://www.proto.utwente.nl/omnomcom/store/')
+    let url = 'https://www.proto.utwente.nl/omnomcom/store/'
+    // Use an argument during startup to determine which store to open.
+    url += app.commandLine.getSwitchValue("store") ?? remote.process.argv['store']
 
-    // Use Ctrl+12 to enable DevTools
+    mainWindow.loadURL(url)
+
+    // Use Ctrl+12 to enable DevTools.
     globalShortcut.register("CmdOrCtrl+F12", () => {
         mainWindow.isFocused() && mainWindow.webContents.toggleDevTools()
     })
 
-    // Press escape to quit application
+    // Press escape to quit application.
     globalShortcut.register("Escape", () => {
         app.quit()
     })
-})
-
-// On OS X it is common for applications and their menu bar
-// to stay active until the user quits explicitly with Cmd + Q
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
 })
